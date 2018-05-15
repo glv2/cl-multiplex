32e7293d7775b89e2fb2c34253285091ac11bdaa
Guillaume LE VAILLANT <glv@posteo.net>
2018-05-15 17:33:20 +0200

    Add write-frame and read-frame functions and use them

--- src/multiplex.lisp
+++ src/multiplex.lisp
@@ -11,9 +11,11 @@
            #:make-multiplex-stream
            #:close-multiplex-stream
            #:with-multiplex-stream
+           #:write-frame
            #:multiplex
            #:finish-multiplex-output
            #:clear-multiplex-input
+           #:read-frame
            #:demultiplex
            #:write-data
            #:read-data
@@ -26,12 +28,8 @@
 (defstruct (multiplex-stream (:constructor %make-multiplex-stream))
   stream
   (buffer (make-array 1024 :element-type '(unsigned-byte 8))
-   :type (simple-array (unsigned-byte 8) (*)))
-  (data (make-array 1024 :element-type '(unsigned-byte 8))
-   :type (simple-array (unsigned-byte 8) (*)))
-  (data-length 0 :type (mod #.array-dimension-limit))
-  (current-frame-channel nil :type (or null unsigned-byte))
-  (current-frame-length nil :type (or null unsigned-byte))
+          :type (simple-array (unsigned-byte 8) (*)))
+  (frame nil :type list)
   inputs
   outputs)
 
@@ -71,6 +69,18 @@ and CHANNELS. The result of the last form of BODY is returned."
           ,@body
        (close-multiplex-stream ,var))))
 
+(defun write-frame (stream channel data &key (start 0) (end (length data)))
+  "Make a frame for a CHANNEL with the bytes of DATA between START and END and
+write it to a STREAM."
+  (flet ((write-integer (n)
+           (do* ((l (max 1 (ceiling (integer-length n) 7)) (1- l))
+                 (x n (ash x -7)))
+                ((zerop l) n)
+             (write-byte (logior (logand x #x7f) (if (= l 1) 0 #x80)) stream))))
+    (write-integer channel)
+    (write-integer (- end start))
+    (write-sequence data stream :start start :end end)))
+
 (defun multiplex (multiplex-stream &optional (max-frame-size 1024))
   "Multiplex the data that was written to the channels of
 MULTIPLEX-STREAM and write it to the underlying stream. The mutiplexed
@@ -82,18 +92,11 @@ MAX-FRAME-SIZE bytes of user data."
     (when (< (length buffer) max-frame-size)
       (setf buffer (make-array max-frame-size :element-type '(unsigned-byte 8)))
       (setf (multiplex-stream-buffer multiplex-stream) buffer))
-    (flet ((write-integer (n)
-             (do* ((l (max 1 (ceiling (integer-length n) 7)) (1- l))
-                   (x n (ash x -7)))
-                  ((zerop l) n)
-               (write-byte (logior (logand x #x7f) (if (= l 1) 0 #x80)) stream))))
-      (dotimes (channel (length outputs) t)
-        (do ((length (read-sequence buffer (aref outputs channel))
-                     (read-sequence buffer (aref outputs channel))))
-            ((zerop length))
-          (write-integer channel)
-          (write-integer length)
-          (write-sequence buffer stream :end length))))))
+    (dotimes (channel (length outputs) t)
+      (do ((length (read-sequence buffer (aref outputs channel))
+                   (read-sequence buffer (aref outputs channel))))
+          ((zerop length))
+        (write-frame stream channel buffer :end length)))))
 
 (defun finish-multiplex-output (multiplex-stream)
   "Multiplex the data that was written to the channels of
@@ -108,17 +111,23 @@ successfully to the underlying stream."
     (dotimes (channel (length inputs))
       (clear-input (aref inputs channel)))))
 
-(defun demultiplex-1 (multiplex-stream max-frame-size)
-  "Read data from the underlying stream of MULTIPLEX-STREAM and
-demultiplex one frame. Return T if a frame was demultiplexed
-successfully, and NIL otherwise."
-  (with-slots (stream
-               data
-               data-length
-               (channel current-frame-channel)
-               (length current-frame-length)
-               inputs)
-      multiplex-stream
+(defun read-frame (stream &key incomplete-frame max-frame-size buffer)
+  "Try to read a frame from a STREAM. The first returned value is a list of
+4 elements representing a frame (channel, total length of data, data, length of
+data received so far). The second returned value is T if the frame is complete
+and NIL otherwise. If INCOMPLETE-FRAME is specified (it must be an incomplete
+frame returned by a previous call to READ-FRAME), the function tries to complete
+it with new data from the STREAM. If MAX-FRAME-SIZE is a positive integer and
+a frame bigger than MAX-FRAME-SIZE is detected, an error is signalled. If BUFFER
+is specified (it must be an array of (UNSIGNED-BYTE 8)), the function tries to
+use it instead of allocating a new work area."
+  (let ((channel (car incomplete-frame))
+        (length (cadr incomplete-frame))
+        (data (or (caddr incomplete-frame)
+                  buffer
+                  (make-array (or max-frame-size 1024)
+                              :element-type '(unsigned-byte 8))))
+        (data-length (or (cadddr incomplete-frame) 0)))
     (flet ((read-integer ()
              (loop
                (let ((b (read-byte stream)))
@@ -137,55 +146,36 @@ successfully, and NIL otherwise."
                         ((= i (1- data-length))
                          (progn
                            (setf data-length 0)
-                           (return-from read-integer n))))))))
-           (reset-demultiplexer ()
-             (setf data-length 0)
-             (setf channel nil)
-             (setf length nil)))
-      (unless channel
-        (setf channel (read-integer))
-        (restart-case
-            (when (>= channel (length inputs))
-              (let ((message (format nil "Bad channel number: ~d (max ~d)"
-                                     channel (1- (length inputs)))))
-                (reset-demultiplexer)
-                (error 'multiplex-error :message message)))
-          (reset-demultiplexer ()
-            :report "Reset the demultiplexer (drop channel number)."
-            (reset-demultiplexer)
-            (return-from demultiplex-1 nil))))
-      (unless length
-        (setf length (read-integer))
-        (restart-case
-            (when (and (integerp max-frame-size) (> length max-frame-size))
-              (let ((message (format nil "Frame too big: ~d bytes (max ~d bytes)"
-                                     length max-frame-size)))
-                (error 'multiplex-error :message message)))
-          (process-frame ()
-            :report "Demultiplex the frame anyway.")
-          (drop-frame ()
-            :report "Read the data of the frame and ignore it."
-            (do ((n length
-                    (- n (read-sequence data stream :end (min n (length data))))))
-                ((zerop n)))
-            (reset-demultiplexer)
-            (return-from demultiplex-1 nil))
-          (reset-demultiplexer ()
-            :report "Reset the demultiplexer (drop channel number and frame length)."
-            (reset-demultiplexer)
-            (return-from demultiplex-1 nil))))
+                           (return-from read-integer n)))))))))
+      (handler-case
+          (progn
+            (unless channel
+              (setf channel (read-integer)))
+            (unless length
+              (setf length (read-integer))
+              (restart-case
+                  (when (and (integerp max-frame-size) (> length max-frame-size))
+                    (let ((message (format nil "Frame too big: ~d bytes (max ~d bytes)"
+                                           length max-frame-size)))
+                      (error 'multiplex-error :message message)))
+                (process-frame ()
+                  :report "Read the frame anyway.")
+                (drop-frame ()
+                  :report "Read the frame and ignore it."
+                  (do ((n length
+                          (- n (read-sequence data stream :end (min n (length data))))))
+                      ((zerop n)))
+                  (return-from read-frame (values nil nil))))))
+        (end-of-file ()
+          (return-from read-frame (values (list channel length data data-length)
+                                          nil))))
       (when (> length (length data))
-        (setf data (make-array length :element-type '(unsigned-byte 8))))
-      (let ((input (aref inputs channel))
-            (n (read-sequence data stream :start data-length :end length)))
-        (if (= n length)
-            (progn
-              (write-sequence data input :end length)
-              (reset-demultiplexer)
-              t)
-            (progn
-              (setf data-length n)
-              nil))))))
+        (let ((new-buffer (make-array length :element-type '(unsigned-byte 8))))
+          (replace new-buffer data :end2 data-length)
+          (setf data new-buffer)))
+      (let ((n (read-sequence data stream :start data-length :end length)))
+        (values (list channel length data n)
+                (= n length))))))
 
 (defun demultiplex (multiplex-stream &optional max-frames max-frame-size)
   "Read data from the underlying stream of MULTIPLEX-STREAM and demultiplex the
@@ -193,20 +183,37 @@ frames. Return T if at least one frame was demultiplexed successfully, and NIL
 otherwise. If MAX-FRAMES is a positive integer, at most MAX-FRAMES frames will
 be demultiplexed. If MAX-FRAME-SIZE is a positive integer and a frame bigger
 than MAX-FRAME-SIZE is detected, an error is signalled."
-  (let ((at-least-one-frame-p nil))
-    (handler-case
-        (do ((frame-p t)
-             (n (when (and (integerp max-frames) (plusp max-frames))
-                  max-frames)))
-            ((or (null frame-p) (and n (zerop n))) at-least-one-frame-p)
-          (setf frame-p (demultiplex-1 multiplex-stream max-frame-size))
-          (when frame-p
-            (when n
-              (decf n))
-            (unless at-least-one-frame-p
-              (setf at-least-one-frame-p t))))
-      (end-of-file (err)
-        (or at-least-one-frame-p (error err))))))
+  (do ((stream (multiplex-stream-stream multiplex-stream))
+       (inputs (multiplex-stream-inputs multiplex-stream))
+       (incomplete-frame (multiplex-stream-frame multiplex-stream))
+       (buffer nil)
+       (at-least-one-frame-p nil)
+       (n (when (and (integerp max-frames) (plusp max-frames))
+            max-frames)
+          (when n
+            (1- n))))
+      ((and n (zerop n)) at-least-one-frame-p)
+    (multiple-value-bind (frame complete-frame-p)
+        (read-frame stream
+                    :incomplete-frame incomplete-frame
+                    :max-frame-size max-frame-size
+                    :buffer buffer)
+      (unless complete-frame-p
+        (setf (multiplex-stream-frame multiplex-stream) frame)
+        (return-from demultiplex at-least-one-frame-p))
+      (destructuring-bind (channel length data data-length)
+          frame
+        (declare (ignore data-length))
+        (when (>= channel (length inputs))
+          (let ((message (format nil "Bad channel number: ~d (max ~d)"
+                                 channel (1- (length inputs)))))
+            (error 'multiplex-error :message message)))
+        (write-sequence data (aref inputs channel) :end length)
+        (setf at-least-one-frame-p t)
+        (setf buffer data)
+        (when incomplete-frame
+          (setf incomplete-frame nil)
+          (setf (multiplex-stream-frame multiplex-stream) nil))))))
 
 (defun write-data (data multiplex-stream channel &key (start 0) end)
   "Like WRITE-SEQUENCE for mutiplex streams. Write the byte of DATA
